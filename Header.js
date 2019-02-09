55364459b46af48929fdc2dadf5eccfca7582f58
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-09 16:08:51 +0100

    Update Header.js

--- Header.js
+++ Header.js
@@ -2,7 +2,12 @@ import React from "react"
 
 function Header(){
  return(
- 
+ <header>
+ <a href="">Home</a>
+ <a href="">Services</a>
+ <a href="">Blog</a>
+ <a href="">Contact</a> 
+ </header>
  ) 
 }
 
