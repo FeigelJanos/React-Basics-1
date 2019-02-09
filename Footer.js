614425fbf362bc5892fdc73da65c828a36e1da0c
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-09 18:27:58 +0100

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -2,7 +2,11 @@ import React from "react"
 
 function Footer(){
  return(
- 
+ <h2>Contact us:</h2>
+ <p>Lorem ipsum: + 12/345-6789</p>
+ <p>Ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+ <a href="">Mail</a>
+ <a href="">Youtube</a>
  ) 
 }
 
