0b145a859d79bdcaa0abf2b19ea15717323aab21
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-10 15:53:36 +0100

    Update ProductCard.js

--- ProductCard.js
+++ ProductCard.js
@@ -1,7 +1,12 @@
 import React from "react"
 
-function ProductCard(){
+function ProductCard(props){
+<div className="card-container" id={props.cardID}>
 
+  <a href={props.linkURL}><img src={props.imgURL} alt={props.imgDescription} /> </a>
+  <p className="product-description">{props.categoryName}</p>
+
+</div>
 }
 
 export default ProductCard
