4549e41c0c8802662262cfedc929626e71b8f9ec
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-10 09:19:08 +0100

    Update MainContent.js

--- MainContent.js
+++ MainContent.js
@@ -2,7 +2,25 @@ import React from "react"
 
 function MainContent(){
  return(
- 
+ <div>
+  <h1>Some Title</h1>
+  
+  <h2>Some subtitle</h2>
+  
+  <p>Latine phaedrum definiebas mea an, ius tota dolorem et, mel et inani munere. Utinam labores admodum an mei. Ad scaevola 
+  reprimique constituam usu, vel ad erant suscipit perpetua, ea duis facete latine sea. Vim ei iuvaret labores eligendi, ne pro 
+  quodsi invenire, mentitum assentior contentiones pri cu.</p>
+  
+  <a><img src="" alt="picture"></a>
+  <a><img src="" alt="picture2"></a>
+  <a><img src="" alt="picture3"></a>
+  
+  <h2>Some subtitle</h2>
+  
+  <p>Est rebum novum qualisque ad, everti eligendi argumentum cum in. Et duo tale omnis mentitum, ne eam euismod postulant torquatos.
+  Ad ius nullam audiam nominavi, alienum constituam ne vix. Eu sit oratio vocibus vivendum, vim ex quodsi vituperatoribus,
+  tamquam blandit necessitatibus eu duo. Et pri ridens labore, in per stet nullam fuisset. Similique neglegentur duo cu.</p>
+  </div>
  ) 
 }
 
